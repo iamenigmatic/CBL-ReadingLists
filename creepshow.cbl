e91a2fb4390cce881f3f3cade6164c3828011e3f
iamenigmatic <78707611+iamenigmatic@users.noreply.github.com>
2026-03-09 06:15:26 -0700

    added creepshow

---
+++ creepshow.cbl
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>Creepshow</Name>
+<NumIssues>26</NumIssues>
+<Books>
+<Book Series="Stephen King's Creepshow" Number="1" Volume="1982" Year="1982">
+<Database Name="cv" Series="23367" Issue="140230" />
+</Book>
+<Book Series="Creepshow" Number="1" Volume="1" Year="2022">
+<Database Name="cv" Series="145224" Issue="948700" />
+</Book>
+<Book Series="Creepshow" Number="2" Volume="1" Year="2022">
+<Database Name="cv" Series="145224" Issue="952409" />
+</Book>
+<Book Series="Creepshow" Number="3" Volume="1" Year="2022">
+<Database Name="cv" Series="145224" Issue="957642" />
+</Book>
+<Book Series="Creepshow" Number="4" Volume="1" Year="2022">
+<Database Name="cv" Series="145224" Issue="962092" />
+</Book>
+<Book Series="Creepshow" Number="5" Volume="1" Year="2022">
+<Database Name="cv" Series="145224" Issue="965953" />
+</Book>
+<Book Series="Creepshow" Number="1" Volume="2" Year="2023">
+<Database Name="cv" Series="153969" Issue="1021560" />
+</Book>
+<Book Series="Creepshow" Number="2" Volume="2" Year="2023">
+<Database Name="cv" Series="153969" Issue="1024654" />
+</Book>
+<Book Series="Creepshow" Number="3" Volume="2" Year="2023">
+<Database Name="cv" Series="153969" Issue="1029690" />
+</Book>
+<Book Series="Creepshow" Number="4" Volume="2" Year="2023">
+<Database Name="cv" Series="153969" Issue="1036511" />
+</Book>
+<Book Series="Creepshow: Holiday Special 2023" Number="1" Volume="2023" Year="2023">
+<Database Name="cv" Series="155174" Issue="1033415" />
+</Book>
+<Book Series="Creepshow" Number="5" Volume="2" Year="2024">
+<Database Name="cv" Series="153969" Issue="1041293" />
+</Book>
+<Book Series="Creepshow: Joe Hill's Wolverton Station" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="156861" Issue="1044953" />
+</Book>
+<Book Series="Creepshow" Number="1" Volume="3" Year="2024">
+<Database Name="cv" Series="160094" Issue="1071739" />
+</Book>
+<Book Series="Creepshow" Number="2" Volume="3" Year="2024">
+<Database Name="cv" Series="160094" Issue="1074740" />
+</Book>
+<Book Series="Creepshow" Number="3" Volume="3" Year="2024">
+<Database Name="cv" Series="160094" Issue="1079380" />
+</Book>
+<Book Series="Creepshow" Number="4" Volume="3" Year="2024">
+<Database Name="cv" Series="160094" Issue="1086199" />
+</Book>
+<Book Series="Creepshow: Holiday Special 2024" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="161627" Issue="1081701" />
+</Book>
+<Book Series="Creepshow" Number="5" Volume="3" Year="2025">
+<Database Name="cv" Series="160094" Issue="1091420" />
+</Book>
+<Book Series="Creepshow in Love" Number="1" Volume="2025" Year="2025">
+<Database Name="cv" Series="162410" Issue="1095695" />
+</Book>
+<Book Series="Creepshow" Number="1" Volume="4" Year="2025">
+<Database Name="cv" Series="167084" Issue="1133797" />
+</Book>
+<Book Series="Creepshow" Number="2" Volume="4" Year="2025">
+<Database Name="cv" Series="167084" Issue="1139140" />
+</Book>
+<Book Series="Super Creepshow Special" Number="1" Volume="2025" Year="2025">
+<Database Name="cv" Series="167515" Issue="1137180" />
+</Book>
+<Book Series="Creepshow" Number="3" Volume="4" Year="2025">
+<Database Name="cv" Series="167084" Issue="1144219" />
+</Book>
+<Book Series="Creepshow" Number="4" Volume="4" Year="2025">
+<Database Name="cv" Series="167084" Issue="1149830" />
+</Book>
+<Book Series="Creepshow" Number="5" Volume="4" Year="2026">
+<Database Name="cv" Series="167084" Issue="1154081" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
