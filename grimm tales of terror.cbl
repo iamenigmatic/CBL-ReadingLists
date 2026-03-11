f15bababad9ed0b57b100873c4af6c744e59bb21
iamenigmatic <78707611+iamenigmatic@users.noreply.github.com>
2026-03-11 02:09:15 -0700

    added Grimm Tales of Terror

---
+++ grimm tales of terror.cbl
@@ -0,0 +1,272 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ReadingList xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+<Name>Grimm Tales of Terror</Name>
+<NumIssues>26</NumIssues>
+<Books>
+<Book Series="Grimm Tales of Terror" Number="1" Volume="1" Year="2014">
+<Database Name="cv" Series="76097" Issue="461029" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="2" Volume="1" Year="2014">
+<Database Name="cv" Series="76097" Issue="464052" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="3" Volume="1" Year="2014">
+<Database Name="cv" Series="76097" Issue="465315" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="4" Volume="1" Year="2014">
+<Database Name="cv" Series="76097" Issue="467729" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="5" Volume="1" Year="2014">
+<Database Name="cv" Series="76097" Issue="472023" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="6" Volume="1" Year="2015">
+<Database Name="cv" Series="76097" Issue="475517" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="7" Volume="1" Year="2015">
+<Database Name="cv" Series="76097" Issue="478659" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="8" Volume="1" Year="2015">
+<Database Name="cv" Series="76097" Issue="480036" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="9" Volume="1" Year="2015">
+<Database Name="cv" Series="76097" Issue="481819" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="10" Volume="1" Year="2015">
+<Database Name="cv" Series="76097" Issue="485880" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="11" Volume="1" Year="2015">
+<Database Name="cv" Series="76097" Issue="489967" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="12" Volume="1" Year="2015">
+<Database Name="cv" Series="76097" Issue="493181" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="13" Volume="1" Year="2015">
+<Database Name="cv" Series="76097" Issue="496461" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="1" Volume="2" Year="2015">
+<Database Name="cv" Series="85478" Issue="503788" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="2" Volume="2" Year="2015">
+<Database Name="cv" Series="85478" Issue="505608" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="3" Volume="2" Year="2015">
+<Database Name="cv" Series="85478" Issue="508608" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="4" Volume="2" Year="2016">
+<Database Name="cv" Series="85478" Issue="512498" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="5" Volume="2" Year="2016">
+<Database Name="cv" Series="85478" Issue="517887" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="6" Volume="2" Year="2016">
+<Database Name="cv" Series="85478" Issue="522473" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="7" Volume="2" Year="2016">
+<Database Name="cv" Series="85478" Issue="527639" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="8" Volume="2" Year="2016">
+<Database Name="cv" Series="85478" Issue="529016" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="9" Volume="2" Year="2016">
+<Database Name="cv" Series="85478" Issue="534348" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="10" Volume="2" Year="2016">
+<Database Name="cv" Series="85478" Issue="538725" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="11" Volume="2" Year="2016">
+<Database Name="cv" Series="85478" Issue="546106" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="12" Volume="2" Year="2016">
+<Database Name="cv" Series="85478" Issue="548663" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="13" Volume="2" Year="2016">
+<Database Name="cv" Series="85478" Issue="552238" />
+</Book>
+<Book Series="Grimm Tales of Terror 2016 Holiday Special" Number="1" Volume="2016" Year="2016">
+<Database Name="cv" Series="96142" Issue="562694" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="1" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="578525" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="2" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="582610" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="3" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="587472" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="4" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="591825" />
+</Book>
+<Book Series="Grimm Tales of Terror April Fool's 2017 Edition" Number="1" Volume="2017" Year="2017">
+<Database Name="cv" Series="100576" Issue="590870" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="5" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="598438" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="6" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="601856" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="7" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="608294" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="8" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="615061" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="9" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="625387" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="10" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="641479" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="11" Volume="3" Year="2017">
+<Database Name="cv" Series="98336" Issue="648016" />
+</Book>
+<Book Series="Grimm Tales of Terror Black & White Special Edition" Number="1" Volume="2018" Year="2018">
+<Database Name="cv" Series="108250" Issue="656755" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="12" Volume="3" Year="2018">
+<Database Name="cv" Series="98336" Issue="660732" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="13" Volume="3" Year="2018">
+<Database Name="cv" Series="98336" Issue="661209" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="1" Volume="4" Year="2018">
+<Database Name="cv" Series="109766" Issue="665985" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="2" Volume="4" Year="2018">
+<Database Name="cv" Series="109766" Issue="670175" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="3" Volume="4" Year="2018">
+<Database Name="cv" Series="109766" Issue="670799" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="4" Volume="4" Year="2018">
+<Database Name="cv" Series="109766" Issue="675106" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="5" Volume="4" Year="2018">
+<Database Name="cv" Series="109766" Issue="676610" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="6" Volume="4" Year="2018">
+<Database Name="cv" Series="109766" Issue="683786" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="7" Volume="4" Year="2018">
+<Database Name="cv" Series="109766" Issue="685733" />
+</Book>
+<Book Series="Grimm Tales of Terror 2018 Halloween Special" Number="1" Volume="2018" Year="2018">
+<Database Name="cv" Series="113478" Issue="684862" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="8" Volume="4" Year="2018">
+<Database Name="cv" Series="109766" Issue="690701" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="9" Volume="4" Year="2018">
+<Database Name="cv" Series="109766" Issue="693404" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="10" Volume="4" Year="2018">
+<Database Name="cv" Series="109766" Issue="695592" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="11" Volume="4" Year="2019">
+<Database Name="cv" Series="109766" Issue="697563" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="12" Volume="4" Year="2019">
+<Database Name="cv" Series="109766" Issue="700614" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="13" Volume="4" Year="2019">
+<Database Name="cv" Series="109766" Issue="703022" />
+</Book>
+<Book Series="Grimm Tales of Terror presents The Bridgewater Triangle" Number="1" Volume="2019" Year="2019">
+<Database Name="cv" Series="121570" Issue="720153" />
+</Book>
+<Book Series="Grimm Tales of Terror 2019 Halloween Special" Number="1" Volume="2019" Year="2019">
+<Database Name="cv" Series="122064" Issue="723102" />
+</Book>
+<Book Series="Grimm Tales of Terror presents The Bridgewater Triangle" Number="2" Volume="2019" Year="2019">
+<Database Name="cv" Series="121570" Issue="725169" />
+</Book>
+<Book Series="Grimm Tales of Terror presents The Bridgewater Triangle" Number="3" Volume="2019" Year="2019">
+<Database Name="cv" Series="121570" Issue="731420" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: Hellfire" Number="1" Volume="2020" Year="2020">
+<Database Name="cv" Series="130760" Issue="805087" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: 2020 Halloween Special" Number="1" Volume="2020" Year="2020">
+<Database Name="cv" Series="131346" Issue="813114" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: H.H. Holmes" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="135275" Issue="842111" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: Game Night" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="137757" Issue="873262" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: 2021 Halloween Special" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="139736" Issue="890755" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: Bachelorette Party" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="140130" Issue="894361" />
+</Book>
+<Book Series="Grimm Tales of Terror Annual: Goddess of Death" Number="1" Volume="2021" Year="2021">
+<Database Name="cv" Series="140154" Issue="894583" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: Sea of Souls" Number="1" Volume="2022" Year="2022">
+<Database Name="cv" Series="142580" Issue="919318" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: Back To School" Number="1" Volume="2022" Year="2022">
+<Database Name="cv" Series="143879" Issue="935194" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: Rise of Cthulhu" Number="1" Volume="2022" Year="2022">
+<Database Name="cv" Series="145370" Issue="949664" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: 2022 Holiday Special" Number="1" Volume="2022" Year="2022">
+<Database Name="cv" Series="147104" Issue="961769" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: 2023 Valentine's Day Special" Number="1" Volume="2023" Year="2023">
+<Database Name="cv" Series="148250" Issue="971557" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: Heart's Desire" Number="1" Volume="2023" Year="2023">
+<Database Name="cv" Series="149611" Issue="984763" />
+</Book>
+<Book Series="Grimm Tales of Terror Quarterly: 2023 Halloween Special" Number="1" Volume="2023" Year="2023">
+<Database Name="cv" Series="154589" Issue="1028863" />
+</Book>
+<Book Series="Grimm Tales of Terror 2024 Holiday Special" Number="1" Volume="2024" Year="2024">
+<Database Name="cv" Series="161634" Issue="1081711" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="1" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1090367" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="2" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1095703" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="3" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1098432" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="4" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1101484" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="5" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1109709" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="6" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1115810" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="7" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1124878" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="8" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1127288" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="9" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1135535" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="10" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1140072" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="11" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1153122" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="12" Volume="5" Year="2025">
+<Database Name="cv" Series="162017" Issue="1153123" />
+</Book>
+<Book Series="Grimm Tales of Terror" Number="13" Volume="5" Year="2026">
+<Database Name="cv" Series="162017" Issue="" />
+</Book>
+</Books>
+<Matchers />
+</ReadingList>
